cf49269539dc2728ce52596a42d53baa57af4197
Tajerbashi <kamrantajerbashi@gmail.com>
2023-02-20 00:43:37 +0330

    Make Partial Views

---
+++ WebBlog/Pages/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebBlog.Pages
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
