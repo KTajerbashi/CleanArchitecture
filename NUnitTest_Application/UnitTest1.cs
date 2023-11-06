c5ec260b978cb53919d42d2eb6fdff519bc04f46
KTajerbsahi <kamrantajerbashi@gmail.com>
2023-11-07 02:04:53 +0330

    Unit Test Project Added

---
+++ NUnitTest_Application/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace NUnitTest_Application
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ XUnitTest_Application/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace XUnitTest_Application
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
