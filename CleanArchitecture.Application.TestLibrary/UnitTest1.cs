f910b9b8dc0eb4583c336d38a345bb2294fe0c25
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-04-13 03:22:09 +0330

    Create New Project

---
+++ CleanArchitecture.Application.TestLibrary/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitecture.Application.TestLibrary
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CleanArchitecture.Domain.TestLibrary/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitecture.Domain.TestLibrary
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
