92ff96c96c720a47fd98332474147cb82109bae1
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-04-27 21:28:10 +0330

    Fix : Check Test Libraries

--- CleanArchitecture.Application.TestLibrary/Services/Security/Fixture/SecurityFixtureCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CleanArchitecture.Application.TestLibrary.Services.Security.Fixture
-{
-    public class SecurityFixtureCollection : ICollectionFixture<SecurityFixture>
-    {
-
-    }
-}
