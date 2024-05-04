b7801004ead5f7a4ca2269e144ba9dea260822f8
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-05-04 23:32:30 +0330

    Sync

--- CleanArchitecture.Persistance.Library/Migrations/20240503190602_Add_Constance_Schema.Designer.cs
+++ CleanArchitecture.Persistance.Library/Migrations/20240503190602_Add_Constance_Schema.Designer.cs
@@ -4,6 +4,7 @@ using CleanArchitecture.Persistence.Library.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitecture.Persistence.Library.Migrations
 {
     [DbContext(typeof(DBContextApplication))]
-    partial class DBContextApplicationModelSnapshot : ModelSnapshot
+    [Migration("20240503190602_Add_Constance_Schema")]
+    partial class Add_Constance_Schema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CleanArchitecture.Persistance.Library/Migrations/20240503190602_Add_Constance_Schema.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Persistence.Library.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Constance_Schema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_NationalCode",
+                schema: "Security",
+                table: "Users");
+
+            migrationBuilder.EnsureSchema(
+                name: "Constance");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SignFile",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalCode",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarFile",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "Security",
+                table: "Roles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "Constance",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateByID = table.Column<long>(type: "bigint", nullable: false),
+                    UpdateByID = table.Column<long>(type: "bigint", nullable: false),
+                    DeleteByID = table.Column<long>(type: "bigint", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeleteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MenuLinks",
+                schema: "Constance",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Icon = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParentId = table.Column<long>(type: "bigint", nullable: false),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateByID = table.Column<long>(type: "bigint", nullable: false),
+                    UpdateByID = table.Column<long>(type: "bigint", nullable: false),
+                    DeleteByID = table.Column<long>(type: "bigint", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeleteDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuLinks", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_MenuLinks_MenuLinks_ParentId",
+                        column: x => x.ParentId,
+                        principalSchema: "Constance",
+                        principalTable: "MenuLinks",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Variables",
+                schema: "Constance",
+                columns: table => new
+                {
+                    ID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ParentId = table.Column<long>(type: "bigint", nullable: false),
+                    ChildrenID = table.Column<long>(type: "bigint", nullable: true),
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreateByID = table.Column<long>(type: "bigint", nullable: false),
+                    UpdateByID = table.Column<long>(type: "bigint", nullable: false),
+                    DeleteByID = table.Column<long>(type: "bigint", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeleteDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Variables", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Variables_Variables_ChildrenID",
+                        column: x => x.ChildrenID,
+                        principalSchema: "Constance",
+                        principalTable: "Variables",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_NationalCode",
+                schema: "Security",
+                table: "Users",
+                column: "NationalCode",
+                unique: true,
+                filter: "[NationalCode] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuLinks_ParentId",
+                schema: "Constance",
+                table: "MenuLinks",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Variables_ChildrenID",
+                schema: "Constance",
+                table: "Variables",
+                column: "ChildrenID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "Constance");
+
+            migrationBuilder.DropTable(
+                name: "MenuLinks",
+                schema: "Constance");
+
+            migrationBuilder.DropTable(
+                name: "Variables",
+                schema: "Constance");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_NationalCode",
+                schema: "Security",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SignFile",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalCode",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AvatarFile",
+                schema: "Security",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                schema: "Security",
+                table: "Roles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_NationalCode",
+                schema: "Security",
+                table: "Users",
+                column: "NationalCode",
+                unique: true);
+        }
+    }
+}
