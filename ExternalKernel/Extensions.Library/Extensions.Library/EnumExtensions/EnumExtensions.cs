c92a9ab75bc7a066d1043371973d7c45cfca0b4a
KTajerbsahi <kamrantajerbashi@gmail.com>
2024-07-29 02:00:12 +0330

    Add Extension

---
+++ ExternalKernel/Extensions.Library/Extensions.Library/EnumExtensions/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Extensions.Library.EnumExtensions;
+
+public static class EnumExtensions
+{
+    public static string GetDescription(this Enum value)
+    {
+        var field = value.GetType().GetField(value.ToString());
+        var attribute = field.GetCustomAttributes(typeof(DescriptionAttribute), false).FirstOrDefault() as DescriptionAttribute;
+        return attribute == null ? value.ToString() : attribute.Description;
+    }
+}
