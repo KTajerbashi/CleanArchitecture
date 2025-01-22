94258f33dc55b0e11798e3121afa840871d06872
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-22 21:30:24 +0330

    Feature : Add Base Infrastructure

---
+++ Src/2.Infrastructure/CleanArchitecture.Infra.SqlServer.Library/BaseInfrastructure/BaseDatabaseContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infra.SqlServer.Library.BaseInfrastructure;
+
+public abstract class BaseDatabaseContext : IdentityDbContext
+{
+}
+
