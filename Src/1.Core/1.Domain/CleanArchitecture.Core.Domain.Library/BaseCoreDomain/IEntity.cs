195b06dba40f3e39f99923f399adb333201ae859
KTajerbashi <kamrantajerbashi@gmail.com>
2025-01-22 21:30:06 +0330

    Feature : Add Base Core Domain and Application

---
+++ Src/1.Core/1.Domain/CleanArchitecture.Core.Domain.Library/BaseCoreDomain/IEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace CleanArchitecture.Core.Domain.Library.BaseCoreDomain;
+
+public interface IEntity
+{
+}
+
+public abstract class Entity : IEntity
+{
+
+}
+
+public interface IAuditableEntity : IEntity
+{
+}
+
+
+public abstract class AuditableEntity : Entity, IAuditableEntity
+{
+
+}
---
+++ Src/1.Core/2.Application/CleanArchitecture.Core.Application.Library/BaseCoreApplication/IBaseRepository.cs
@@ -0,0 +1,7 @@
+﻿using CleanArchitecture.Core.Domain.Library.BaseCoreDomain;
+
+namespace CleanArchitecture.Core.Application.Library.BaseCoreApplication;
+
+public interface IBaseRepository<TEntity, TId> where TEntity : class, IAuditableEntity
+{
+}
