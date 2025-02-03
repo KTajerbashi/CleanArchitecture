a561c0d5201c35cf6c69230ef89042f5db132d04
KTajerbashi <kamrantajerbashi@gmail.com>
2025-02-03 23:29:02 +0330

    Feature : Add Identity Entity Domain

---
+++ Src/1.Core/1.Domain/CleanArchitecture.Core.Domain.Library/Identity/UserClaimEntity.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitecture.Core.Domain.Library.Common;
+using Microsoft.AspNetCore.Identity;
+
+namespace CleanArchitecture.Core.Domain.Library.Identity;
+
+public class UserClaimEntity : IdentityUserClaim<long>, IAuditableEntity<long>
+{
+
+    public DateTime CreatedDate { get; private set; }
+
+    public long CreatedByUserRoleId { get; private set; }
+
+    public DateTime? UpdatedDate { get; private set; }
+
+    public long UpdatedByUserRoleId { get; private set; }
+
+    public Guid EntityId { get; private set; }
+    public long Id { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public void Access()
+    {
+        IsActive = true;
+        IsDeleted = false;
+    }
+    public void Delete()
+    {
+        IsActive = false;
+        IsDeleted = true;
+    }
+    public UserClaimEntity()
+    {
+        
+    }
+}
---
+++ Src/1.Core/1.Domain/CleanArchitecture.Core.Domain.Library/Identity/UserEntity.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchitecture.Core.Domain.Library.Common;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchitecture.Core.Domain.Library.Identity;
+
+[Table("Users", Schema = "Identity")]
+public class UserEntity : IdentityUser<long>, IAuditableEntity<long>
+{
+    
+    [Description("")]
+    public string? Name { get; private set; }
+    [Description("")]
+    public string? Family { get; private set; }
+    [Description("")]
+    public string? DisplayName { get; private set; }
+    [Description("")]
+    public string? PersonalCode { get; private set; }
+
+    [Description("")]
+    public DateTime CreatedDate { get; private set; }
+    [Description("")]
+    public long CreatedByUserRoleId { get; private set; }
+    [Description("")]
+    public DateTime? UpdatedDate { get; private set; }
+    [Description("")]
+    public long UpdatedByUserRoleId { get; private set; }
+    [Description("")]
+    public Guid EntityId { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+    public bool IsActive { get; private set; }
+
+    public void Access()
+    {
+        IsActive = true;
+        IsDeleted = false;
+    }
+    public void Delete()
+    {
+        IsActive = false;
+        IsDeleted = true;
+    }
+    public UserEntity()
+    {
+
+    }
+
+}
