77594adc8b83d5f3aab027879a68ec475c50aad1
KTajerbashi <kamrantajerbashi@gmail.com>
2025-05-01 21:29:57 +0330

    Feature : Dapper Run Query Completed

---
+++ Src/1.Core/2.Application/CleanArchitecture.Core.Application.Library/Providers/DataDapper/IDataDapper.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace CleanArchitecture.Core.Application.Library.Providers.DataDapper;
+
+public interface IDataDapper
+{
+    Task<TModel> ReadAsync<TModel>(string query, object parameters = null!, IDbTransaction transaction = null!);
+    Task<IEnumerable<TModel>> ReadListAsync<TModel>(string query, object parameters = null!, IDbTransaction transaction = null!);
+    Task<int> ExecuteAsync(string command, object parameters = null!, IDbTransaction transaction = null!);
+    Task<T> ExecuteScalarAsync<T>(string command, object parameters = null!, IDbTransaction transaction = null!);
+    IDbTransaction BeginTransaction();
+}
--- Src/1.Core/2.Application/CleanArchitecture.Core.Application.Library/Providers/ProviderServices.cs
+++ Src/1.Core/2.Application/CleanArchitecture.Core.Application.Library/Providers/ProviderServices.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchitecture.Core.Application.Library.Providers.CacheSystem;
+using CleanArchitecture.Core.Application.Library.Providers.DataDapper;
 using CleanArchitecture.Core.Application.Library.Providers.ObjectMapper;
 using CleanArchitecture.Core.Application.Library.Providers.Serializer.Objects;
 using CleanArchitecture.Core.Application.Library.Providers.UserManagement;
@@ -11,13 +12,21 @@ public class ProviderServices
     public readonly IObjectMapper Mapper;
     public readonly ICacheAdapter CacheAdapter;
     public readonly IMediator Mediator;
+    public readonly IDataDapper DataDapper;
     public readonly IUser User;
-    public ProviderServices(IObjectSerializer convertor, IObjectMapper mapper, ICacheAdapter cacheAdapter, IMediator mediator, IUser user)
+    public ProviderServices(
+        IObjectSerializer convertor, 
+        IObjectMapper mapper, 
+        ICacheAdapter cacheAdapter, 
+        IMediator mediator, 
+        IUser user, 
+        IDataDapper dataDapper)
     {
         Convertor = convertor;
         Mapper = mapper;
         CacheAdapter = cacheAdapter;
         Mediator = mediator;
         User = user;
+        DataDapper = dataDapper;
     }
 }
