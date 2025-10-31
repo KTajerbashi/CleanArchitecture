d197eb17edd7d9dc0a3e2aa9e898a912b47fcf2c
KTajerbashi <kamrantajerbashi@gmail.com>
2025-10-31 16:24:48 +0330

    #Feature Update Packages Solution

--- Src/1.Core/CleanArchitecture.Core.Application/Providers/ObjectMapper/DependencyInjection.cs
+++ Src/1.Core/CleanArchitecture.Core.Application/Providers/ObjectMapper/DependencyInjection.cs
@@ -6,31 +6,36 @@ namespace CleanArchitecture.Core.Application.Providers.ObjectMapper;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddAutoMapperProfiles(this IServiceCollection services,IConfiguration configuration,string sectionName)
+    public static IServiceCollection AddAutoMapperProfiles(this IServiceCollection services, IConfiguration configuration, string sectionName)
         => services.AddAutoMapperProfiles(configuration.GetSection(sectionName));
 
     public static IServiceCollection AddAutoMapperProfiles(this IServiceCollection services, IConfiguration configuration)
     {
         var option = configuration.Get<ObjectMapperOption>();
 
-        var assembliesLibrary = option.Assemblies.Split(",");
-        foreach (var item in assembliesLibrary)
+        var assembliesLibrary = option?.Assemblies.Split(",");
+        foreach (var item in assembliesLibrary!)
         {
-            var assemblies = item.GetAssemblies();
+            var assemblies = item.GetAssemblies().ToArray();
             services.AddAutoMapper(assemblies);
-
-            services.AddSingleton<IObjectMapper, ObjectMapperService>();
         }
 
+        services.AddSingleton<IObjectMapper, ObjectMapperService>();
         return services;
     }
+
     public static IServiceCollection AddAutoMapperProfiles(this IServiceCollection services, Action<ObjectMapperOption> setupAction)
     {
         var option = new ObjectMapperOption();
         setupAction.Invoke(option);
 
-        var assemblies = option.Assemblies.GetAssemblies();
+        var assemblies = option.Assemblies.GetAssemblies().ToArray();
 
-        return services.AddAutoMapper(assemblies).AddSingleton<IObjectMapper, ObjectMapperService>();
+        services.AddAutoMapper(assemblies);
+        services.AddSingleton<IObjectMapper, ObjectMapperService>();
+
+        return services;
     }
 }
+
+
